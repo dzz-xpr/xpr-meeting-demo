c7c2957b7c0b65a7e60ede4b651cdf8bb9855ec5
dzz <devon.zachary@gmail.com>
2020-03-05 22:16:58 -0800

    tweak paths

--- xpr/server_js/template/local.js
+++ xpr/server_js/template/local.js
@@ -1,3 +1,5 @@
+//example of a local path reference
+
 export.process = function(context, options) {
     return { "Hello" : "World" };
 }
--- xpr/server_js/utils/to_uppercase.js
+++ xpr/server_js/utils/to_uppercase.js
@@ -1,3 +1,5 @@
+//for later testing with "/" prefixed paths
+
 export.process = function(context, options) {
     return { "Hello" : "World" };
 }
