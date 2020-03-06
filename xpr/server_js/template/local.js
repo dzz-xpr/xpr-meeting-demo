372cd3d5e369516fddf13fa9c905f9728b96e7cb
dzz <devon.zachary@gmail.com>
2020-03-06 00:30:53 -0800

    test

--- xpr/server_js/template/local.js
+++ xpr/server_js/template/local.js
@@ -1,4 +1,4 @@
-let upper_array = require("../modules/upper_array");
+let upper_array = require("../modules/upper_array").upper_array;
 
 exports.process = function(context, options) {
     return upper_array( context, options["field"] );
