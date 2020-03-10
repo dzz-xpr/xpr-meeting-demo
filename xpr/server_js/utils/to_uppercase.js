b6998b869d5b2c12c9c395663d2ac78ecb51f049
dzz <devon.zachary@gmail.com>
2020-03-09 18:05:49 -0700

    update

--- xpr/server_js/utils/to_uppercase.js
+++ xpr/server_js/utils/to_uppercase.js
@@ -1,5 +1,5 @@
 //for later testing with "/" prefixed paths
-let upper_array = require("/modules/upper_array").upper_array;
+let upper_array = require("../modules/upper_array").upper_array;
 exports.process = function(context, options) {
     return upper_array( context, options["field"] );
 }
