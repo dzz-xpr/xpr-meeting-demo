571205c76cdafeab60c25a9a28f0ff3c6a8b2d3f
dzz <devon.zachary@gmail.com>
2020-03-12 21:37:52 -0700

    update

--- xpr/server_js/utils/add_user.js
+++ xpr/server_js/utils/add_user.js
@@ -1,6 +1,9 @@
-let api = require("/xpr/request").XprApi;
+let request = require("/xpr/request");
 
 exports.process = function(context, options) {
-
-
+    return request.XprApi({
+        "method" : "GET",
+        "uri" : "/articles/",
+        "params" : {}, "data" : {}
+    }); 
 }
